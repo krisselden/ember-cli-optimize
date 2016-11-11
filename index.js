4dfdc0ea9b43560c92cc7b0f424bfffeab6569db
Kris Selden <kris.selden@gmail.com>
2016-11-10 23:57:30 -0800

    add logging

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 /* jshint node: true */
 'use strict';
 
+var logger = require('heimdalljs-logger')('ember-cli-optimize');
+
 module.exports = {
   name: 'ember-cli-optimize',
   included: function(app) {
-    this.options = app.options.optimize || {};
+    this.app = app;
+    var options = app.options.optimize || {};
+    if (options.enabled === undefined) {
+      options.enabled = app.env === 'production';
+    }
+    logger.trace('included ' + app.project.name());
+    this.options = options;
+    if (app.options.sourcemaps && app.options.sourcemaps.enabled) {
+      logger.warn('does not work with sourcemaps currently');
+    }
   },
   postprocessTree: function(type, tree) {
+    var app = this.app;
     var options = this.options;
-    if(options.enabled === true && type === 'all') {
+    if(options.enabled && type === 'all') {
+      logger.trace('postprocessTree ' + app.project.name());
       return require('./lib/optimize')(tree, options);
     }
     return tree;
